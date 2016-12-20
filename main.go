cc2fbc86f573dd5e27647579af0cabb1306045ce
Kaleb Olson <kaleb.olson@gmail.com>
2016-12-20 11:32:14 -0700

    fixed issue with printf

--- main.go
+++ main.go
@@ -11,9 +11,8 @@ func main() {
 	modePtr := flag.String("mode", "random", "Mode to use.")
 	flag.Parse()
 
-	fmt.Fprintln("Using mode: %s", *modePtr)
+	fmt.Printf("Using mode: %s\n", *modePtr)
 
 	robotgo.ScrollMouse(10, "up")
-	robotgo.MouseClick("left", true)
 	robotgo.MoveMouseSmooth(100, 200, 1.0, 100.0)
 }
