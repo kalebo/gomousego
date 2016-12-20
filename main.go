a9a4c569d65371bbee68b86471b4c78eebcdef68
Kaleb Olson <kalebo@byu.local>
2016-12-20 10:55:53 -0700

    Added flag parsing

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+
 	"github.com/go-vgo/robotgo"
 )
 
 func main() {
+	modePtr := flag.String("mode", "random", "Mode to use.")
+	flag.Parse()
+
+	fmt.Fprintln("Using mode: %s", *modePtr)
+
 	robotgo.ScrollMouse(10, "up")
-	robotgo.MouseClick("left",true)
+	robotgo.MouseClick("left", true)
 	robotgo.MoveMouseSmooth(100, 200, 1.0, 100.0)
 }
